e2d77dec28a514463bf7a84510e3986c75238c23
Iqranaz993 <iqran9935@gmail.com>
2024-04-02 00:14:12 +0500

    this is the simple cli number guessing game

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ const answer = await inquirer.prompt([{
     message: "please guess a number between 1-6:",
 },
 ]);
+
 if (answer.numbergusses === randomNumber){
     console.log("congratulations! you guess right number");
 } else {
